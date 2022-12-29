49ea70c18125ba04366d91dbb1785bbe4dfcfa68
jin <yiming.jin@yingxiong.com>
2022-12-29 18:09:23 +0800

    更新

---
+++ Client/Assets/Scr/FrameWork/Network/Connect/NetworkCounter.cs
@@ -0,0 +1,11 @@
+﻿namespace GameFrameWork.Network
+{
+    public static class NetworkCounter
+    {
+        public static int s_count = 0;
+        public static int GetCount()
+        {
+            return s_count++;
+        }
+    }
+}
