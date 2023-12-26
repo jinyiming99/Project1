4cfefa71837efe51e17b437bda74f74d7409739a
jin <jinyiming@tal.com>
2023-12-26 13:50:02 +0800

    更新容器

--- Stroage/Assets/Src/Util/Container/LinkList_LowGC.cs
+++ Stroage/Assets/Src/Util/Container/LinkList_LowGC.cs
@@ -1,13 +1,14 @@
 ﻿
+using System.Collections;
 using System.Collections.Generic;
 
-public class LinkList_LowGC<T>
+public class LinkList_LowGC<T> :IEnumerable<T>
 {
     LinkedList<T> _data = new LinkedList<T>();
     Stack<LinkedListNode<T>> _cache = new Stack<LinkedListNode<T>>();
     public int Count => _data.Count;
-    public T First => _data.First == null ? default(T):_data.First.Value;
-    public T Last => _data.Last == null ? default(T):_data.Last.Value;
+    public LinkedListNode<T> First => _data.First;
+    public LinkedListNode<T> Last => _data.Last;
     
     private int _maxCount = 0;
     public int MaxCount
@@ -44,9 +45,42 @@ public class LinkList_LowGC<T>
         _data.AddLast(node);
     }
     
+    public void AddFirst(T value)
+    {
+        if (Contains(value))
+            return;
+        LinkedListNode<T> node;
+        if (_cache.Count > 0)
+        {
+            node = _cache.Pop();
+            node.Value = value;
+        }
+        else
+        {
+            node = new LinkedListNode<T>(value);
+        }
+        _data.AddFirst(node);
+    }
+    
     public bool Contains(T value)
     {
         return _data.Contains(value);
+    }
+    
+    public void RemoveLast()
+    {
+        var node = _data.Last;
+        _data.RemoveLast();
+        if (_cache.Count < _maxCount)
+            _cache.Push(node);
+    }
+    
+    public void RemoveFirst()
+    {
+        var node = _data.First;
+        _data.RemoveFirst();
+        if (_cache.Count < _maxCount)
+            _cache.Push(node);
     }
     
     public void Remove(T value)
@@ -57,9 +91,27 @@ public class LinkList_LowGC<T>
             _cache.Push(node);
     }
     
+    public LinkedListNode<T> Remove(LinkedListNode<T> value)
+    {
+        _data.Remove(value);
+        if (_cache.Count < _maxCount)
+            _cache.Push(value);
+        return value.Next;
+    }
+    
     public void Clear()
     {
         _data.Clear();
         _cache.Clear();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return _data.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
     }
 }
